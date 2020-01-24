1c9bb69a417ee2965fa938d756b1b2ec8c15027e
Stefan Ponik <stefanponik@gmail.com>
2020-01-24 12:13:29 +0000

    rename file and populate vars

--- grafana-dev/grafana_conf.tf
+++ grafana-dev/grafana_conf.tf
@@ -1,13 +1,13 @@
 # VARIABLES
 
 variable "grf_url" {
-  default = "http://localhost:3000"
+#  default = "http://localhost:3000"
 }
 variable "grf_auth" {
-  default = "eyJrIjoic25uOFpHVWVoaTJCM0hhN1BiNzBpUEhseEVINmhiWjAiLCJuIjoiYWRtaW4iLCJpZCI6MX0="
+#  default = "eyJrIjoic25uOFpHVWVoaTJCM0hhN1BiNzBpUEhseEVINmhiWjAiLCJuIjoiYWRtaW4iLCJpZCI6MX0="
 }
 variable "idb_url" {
-  default = "http://influxdb-dev-local:8086"  
+#  default = "http://influxdb-dev-local:8086"  
 }
 
 # PROVIDER
