bb21f93d0d95faefdffd8ee6b2dcc021bc0b0290
Stefan Ponik <stefanponik@gmail.com>
2020-01-30 14:33:02 +0000

    Update tfstate handling with local provider.

--- environments/dev-local/grafana/terraform.tf
+++ environments/dev-local/grafana/terraform.tf
@@ -3,8 +3,11 @@
 #########
 terraform {
   required_version = ">= 0.12.0"
+
   backend "local" {
+    path = ".terraform/tf-state/terraform.tfstate"
   }
+
   required_providers {
     grafana = ">= 1.5"
   }
--- environments/dev-local/influxdb/terraform.tf
+++ environments/dev-local/influxdb/terraform.tf
@@ -3,7 +3,9 @@
 #########
 terraform {
   required_version = ">= 0.12.0"
+
   backend "local" {
+    path = ".terraform/tf-state/terraform.tfstate"
   }
   required_providers {
     influxdb = ">= 1.3"
