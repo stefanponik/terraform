47b27e973fcfdc8c7268be754f1050ac24316496
sponik <sponik@asavie.com>
2020-01-30 14:15:12 +0000

    update structure

--- environments/dev-local/influxdb/variables.tf
+++ environments/dev-local/influxdb/variables.tf
@@ -1,11 +1,11 @@
 ###########
 # Variables
 ###########
+
 variable "idb_url" {
-  default = "http://localhost:8086"
+  type = string
 }
 
 variable "idb_default_db" {
   default = "MetricStore"
 }
-
