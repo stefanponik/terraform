239033ce60bd039f1d334b2ca08d96a86efb415f
sponik <sponik@asavie.com>
2020-02-10 16:06:14 +0000

    update module to use GIT source and TAG

--- environments/dev-local/influxdb/main.tf
+++ environments/dev-local/influxdb/main.tf
@@ -2,8 +2,16 @@
 # Modules
 #########
 
-module "grf-datasources-cfg" {
-  source          = "../../../modules/influxdb/idb-databases-cfg"
+module "terraform-influxdb-databases-cfg" {
+  source          = "github.com/stefanponik/terraform-influxdb-databases-cfg.git?ref=v0.0.1"
   idb_url         = var.idb_url
   database_schema = var.database_schema
 }
+
+
+
+# module "grf-datasources-cfg" {
+#   source          = "../../../modules/influxdb/idb-databases-cfg"
+#   idb_url         = var.idb_url
+#   database_schema = var.database_schema
+# }
